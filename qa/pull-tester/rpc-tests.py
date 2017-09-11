ed62a4dc00562ea4a938e3a6933f8f238f1aeb54
root <root@multiblockchains.com>
2017-09-11 14:19:40 -0500

    replace dash_hash

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -115,14 +115,14 @@ testScripts = [
     'timestampindex.py',
     'spentindex.py',
     'decodescript.py',
-    'p2p-fullblocktest.py', # NOTE: needs mxn_hash to pass
+    'p2p-fullblocktest.py', # NOTE: needs dash_hash to pass
     'blockchain.py',
     'disablewallet.py',
-    'sendheaders.py', # NOTE: needs mxn_hash to pass
+    'sendheaders.py', # NOTE: needs dash_hash to pass
     'keypool.py',
     'prioritise_transaction.py',
-    'invalidblockrequest.py', # NOTE: needs mxn_hash to pass
-    'invalidtxrequest.py', # NOTE: needs mxn_hash to pass
+    'invalidblockrequest.py', # NOTE: needs dash_hash to pass
+    'invalidtxrequest.py', # NOTE: needs dash_hash to pass
     'abandonconflict.py',
     'p2p-versionbits-warning.py',
 ]
@@ -132,9 +132,9 @@ if ENABLE_ZMQ:
 testScriptsExt = [
     'bip9-softforks.py',
     'bip65-cltv.py',
-    'bip65-cltv-p2p.py', # NOTE: needs mxn_hash to pass
+    'bip65-cltv-p2p.py', # NOTE: needs dash_hash to pass
     'bip68-sequence.py',
-    'bipdersig-p2p.py', # NOTE: needs mxn_hash to pass
+    'bipdersig-p2p.py', # NOTE: needs dash_hash to pass
     'bipdersig.py',
     'getblocktemplate_longpoll.py', # FIXME: "socket.error: [Errno 54] Connection reset by peer" on my Mac, same as  https://github.com/bitcoin/bitcoin/issues/6651
     'getblocktemplate_proposals.py',
@@ -146,7 +146,7 @@ testScriptsExt = [
 #    'rpcbind_test.py', #temporary, bug in libevent, see #6655
     'smartfees.py',
     'maxblocksinflight.py',
-    'p2p-acceptblock.py', # NOTE: needs mxn_hash to pass
+    'p2p-acceptblock.py', # NOTE: needs dash_hash to pass
     'mempool_packages.py',
     'maxuploadtarget.py',
     # 'replace-by-fee.py', # RBF is disabled in MXN Core
